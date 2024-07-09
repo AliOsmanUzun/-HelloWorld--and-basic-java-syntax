2be3aba5c48f1c490465b9a63176c83cf276aed9
AliOsmanUzun <aliosmanuzun1@gmail.com>
2024-07-09 13:22:53 +0300

    My First Project
    " Hello World " and basic java syntax

---
+++ 01.firstclass_java/src/ilkders_java1/Merkez.java
@@ -0,0 +1,18 @@
+package ilkders_java1;
+
+public class Merkez {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("Hello World");
+		
+// Yorum satırlarını çift slash ile oluştuyorum.Bunlar compiler tarafından görünmez çalıştırılmaz
+// Belirli bir alanı yorum satırı yapmak istiyorsam /* * / işaretlerini kullanıyorum.
+// String ifadeleri " " tırnak içinde yazıyorum
+// ; noktalı virgül orada işlemin sonlandığını ifade ediyor diğer koda geçiyor compiler
+// Kodlarımı main metodu içinde yazıyorum	
+
+
+	}
+}
